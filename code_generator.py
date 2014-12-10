6e48a789077c5a20e7a0ada9e95ae8f7078fb8df
Sang Ho Lee <stevelee@berkeley.edu>
2014-12-10 13:27:34 -0800

    Code generator skeleton

---
+++ code_generator.py
@@ -0,0 +1,58 @@
+
+class CodeGenerator:
+    def __init__(self, config):
+        self.config = config
+
+    # Separate functionality into different functions and call them here?
+    # Outputs file
+    def generate(self):
+        # May be missing steps...
+        # Create a .c file with the name
+
+        # Include statements
+
+        # For each function:
+
+            # Create function signature with name
+
+            # Instantiate variables
+
+            # Construct format string
+
+            # Construct ParseTuple function
+
+            # Insert 'code begins/ends' demarcations
+
+            # Do decrementing to objects
+
+
+        # Create my methods struct
+
+        # Create intialization function for the module
+
+
+        pass
+
+    def generate_function_block(self, function):
+        pass
+
+    def generate_format_string(self, function):
+        pass
+
+    def generate_variable_instantiations(self, function):
+        pass
+
+    def generate_parse_tuple(self, function):
+        pass
+
+    def generate_decrement(self, function):
+        pass
+
+    def generate_mymethods(self):
+        pass
+
+    def generate_initialization(self):
+        pass
+
+
+
