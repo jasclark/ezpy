bcdff346600a3cf0e90e9a6b168dca5940a9fc30
zarachiara <zarachiara@berkeley.edu>
2014-12-13 15:19:59 -0800

    extension_parser complete
    extension_parser complete

--- build/lib/extension_parser.py
+++ build/lib/extension_parser.py
@@ -1,18 +1,84 @@
 import shelve
-
+import re
 class ExtensionParser:
 
     CONVERSION_DB = 'conversion.db'
 
     # Parses extension configuration file, throws Error if unsuccessful
-    def parse(file_name):
+    def parse(self,file_name):
         extension_file = open(file_name)
-        
-        # TODO: Parse stuff and put it in the DB
+        # extension_dict = {'include':None, 
+        #                     'data_type':None,
+        #                     'num_vars':None, 
+        #                     'format_string': None, 
+        #                     'format_string_args': None,
+        #                     'code': None
+        #                      }
+        extension_dict= {}
 
-        # For now...
-        data_type = None
-        conversion_function = None
+        # TODO: Parse stuff and put it in the DB
+        include_re = re.compile('^include\s*:\s*([a-zA-Z_]+[a-zA-Z0-9_]+[^0-9_]*)$')
+        dataType_re = re.compile('^data_type\s*:\s*([a-zA-Z_]+[a-zA-Z0-9_]*)$')
+        numVars_re = re.compile('^num_vars\s*:\s*([a-zA-Z0-9_]*)$')
+        formatString_re = re.compile('^format_string\s*:\s*([a-zA-Z0-9_]+[^0-9_]*)$')
+        formatStringArgs_re = re.compile('^format_string_args\s*:\s*([^0-9_]+[a-zA-Z0-9_]+[^0-9_]+[a-zA-Z0-9_]*)$')
+        code_re = re.compile('^code\s*:\s*')
+
+        line = extension_file.readline()
+        while line: 
+            line = line.replace(' ', '')
+            line = line.rstrip()
+            print line
+            include_match = include_re.match(line) 
+            if include_match:
+                # print("include_match")
+                # print(include_match)
+                extension_dict['include'] = include_match.group(1)
+
+            dataType_match = dataType_re.match(line)
+            if dataType_match: 
+                print("DATATYPE_MATCH")
+                print(dataType_match)
+                extension_dict['data_type'] = dataType_match.group(1)
+
+            numVars_match = numVars_re.match(line)
+            if numVars_match:
+                extension_dict['num_vars'] = numVars_match.group(1)
+
+            formatString_match = formatString_re.match(line)
+            if formatString_match:
+                extension_dict['format_string'] = formatString_match.group(1)
+
+            # append arguments to a list 
+            # check this 
+            formatStringArgs_match = formatStringArgs_re.match(line)
+            if formatStringArgs_match:
+                print("Argument 1???")
+                args = formatStringArgs_match.group(1)
+                names = args.split(',')
+                extension_dict['format_string_args'] = names
+                # extension_dict['format_string_args'][0] = numVars_match.group(1)
+                # extension_dict['format_string_args'][1] = numVars_match_group(2)
+                print("What is in format_string_arg")
+                print(extension_dict['format_string_args'])
+
+            # put "code" parsing in th dictionary
+            code_match = code_re.match(line)
+            if code_match:
+                print 'here'
+                code_segment = ''
+                line = extension_file.readline()
+                while line:
+                    code_segment += line
+                    line = extension_file.readline()
+                extension_dict['code'] = code_segment
+                print("Where are you code?")
+                print(extension_dict['code'])
+            line = extension_file.readline()
+
+        print("extension_dict")
+        # print extension_dict
+        return extension_dict
 
         # Write out to database
         db = shelve.open(CONVERSION_DB)
--- build/lib/ezpy.py
+++ build/lib/ezpy.py
@@ -1,5 +1,6 @@
 from cliff.command import Command
 from configuration_parser import ConfigurationParser
+from extension_parser import ExtensionParser
 from code_generator import CodeGenerator
 from setup_script_generator import SetupScriptGenerator
 
@@ -21,5 +22,18 @@ class Generate(Command):
 
 
 class Extend(Command):
-	def take_action(self, parsed_args):
-		print("you have invoked extend")
+
+    def get_parser(self, prog_name):
+        parser = super(Extend, self).get_parser(prog_name)
+        parser.add_argument('config_file', nargs='?', default='.')
+        return parser
+
+    def take_action(self, parsed_args):       
+        config_parser = ExtensionParser()
+        config = config_parser.parse(parsed_args.config_file)
+        print config
+        # code_generator = CodeGenerator(config)
+        # code_generator.generate()
+        # setup_script_generator = SetupScriptGenerator()
+        # setup_script_generator.generate(config)
+
